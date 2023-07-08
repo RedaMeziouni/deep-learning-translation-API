8545e9c16ab9c6db8fea16d41a252d908d10a362
RedaMeziouni <meziounidb@gmail.com>
2023-07-08 21:58:52 +0200

    create the store translation function

--- tasks.py
+++ tasks.py
@@ -1,6 +1,14 @@
+from models import TranslationModel
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
 ## Tasks that w will be  running on the Backed
 
 ## Store translation Task => Take translation request and seve it to the DB
+def store_translation(t):
+    model = TranslationModel(text=t.text, base_lang=t.base_lang, final_lang=t.final_lang)
+    model.save()
+    return model.id
 
 ## Run Translation Task => run a deep learning Model
 
