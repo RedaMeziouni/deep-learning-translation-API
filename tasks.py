03bd86286263778553e9045480c6592d748ebe5a
RedaMeziouni <meziounidb@gmail.com>
2023-07-08 21:02:29 +0200

    mockUp the Tasks

---
+++ tasks.py
@@ -0,0 +1,7 @@
+## Tasks that w will be  running on the Backed
+
+## Store translation Task => Take translation request and seve it to the DB
+
+## Run Translation Task => run a deep learning Model
+
+# Find translation Task => Retrieve the translation from the DB
