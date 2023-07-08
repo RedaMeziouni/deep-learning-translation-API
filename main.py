7b2e37efbde295d96bedb4b8b9f08d3588c30f7d
RedaMeziouni <meziounidb@gmail.com>
2023-07-08 20:59:50 +0200

    mockUp the routes

---
+++ main.py
@@ -0,0 +1,5 @@
+## Index Route => Test if this route is working
+
+## Translate Route => Take in a translate request, return a translation, and store it on the DB
+
+## Results route => Take in a translation ID, and return the translated text
